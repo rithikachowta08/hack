e65d994d4f9c18b6dd573b95834c38293ba2097c
rithika <rithzcool@gmail.com>
2018-10-10 08:34:41 +0530

    added python api

--- server.js
+++ server.js
@@ -5,11 +5,30 @@ const cors = require("cors");
 const app = express();
 const port = process.env.PORT || 5000;
 const api = require("./api/routes");
+const cmd = require("node-cmd");
+const fs = require("fs");
 
 app.use(cors());
 app.use(bodyParser.urlencoded({ extended: true }));
 app.use(bodyParser.json());
 app.use("/api", api);
+
+app.post("/execute", (req, res) => {
+  var fileContent = req.body.script;
+  var filepath = "user-script.py";
+
+  fs.writeFile(filepath, fileContent, err => {
+    if (err) throw err;
+    console.log("The file was succesfully saved!");
+    cmd.get("python user-script.py", function(data, err, stderr) {
+      if (!err) {
+        console.log("data from python script " + data);
+      } else {
+        console.log("python script cmd error: " + err);
+      }
+    });
+  });
+});
 
 callJdoodleApi = () => {
   request(
